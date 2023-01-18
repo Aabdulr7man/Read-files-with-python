d8146753c98285034958e3504a842eb4b1aa9b2a
AbdulRahman Khalid <122513436+Aabdulr7man@users.noreply.github.com>
2023-01-18 15:05:33 +0500

    Second method added

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 
 def read_file(path):
-    with open(path, 'r') as file:
+    with open(path, 'r') as file:  # opening the file
         contents = file.read()  # read all file as text (string)
 
         # to see each line as element of list we use
         # file.readlines()
 
-        return contents
+        return contents  # return the contents
 
-
+    
+# File path
 path = "chapter1.txt"
-file_data = read_file(path)
+file_data = read_file(path)  # Calling the function
 
 # print(file_data)
 
-# Reading file the other way
+# Reading file the second way
 
 file = open(path, 'r')  # Open file
 contents = file.read()  # Read the file
 print(contents)  # Printing the contents
 file.close()  # close the file
-
